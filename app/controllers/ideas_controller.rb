c1d2cd3487d6404d6ba2f302ec404da14c4f612c
jugraj <jugraj98123@gmail.com>
2022-03-23 14:52:26 -0700

    added basic crud actions for Ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,6 @@
 class IdeasController < ApplicationController
+
+    before_action :find_idea, only: [:show, :edit, :update, :destroy]
     def new
         @idea  = Idea.new
     end
@@ -9,5 +11,30 @@ class IdeasController < ApplicationController
         else
             render :new
         end
+    end
+    
+    def show
+    end
+
+    def index
+        @ideas = Idea.all.order(created_at: :desc)
+    end
+
+    def edit
+    end
+
+    def update
+        @idea.update params.require(:idea).permit(:title, :description)
+        redirect_to idea_path(@idea)
+    end
+    
+    def destroy
+        @idea.destroy
+        redirect_to ideas_path
+    end
+
+    private
+    def find_idea
+        @idea = Idea.find params[:id]
     end
 end
